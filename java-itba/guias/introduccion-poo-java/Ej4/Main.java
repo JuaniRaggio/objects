1c0c828bdd8c223bcb0335ccb30a94113ba63c74
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-03-21 15:56:59 -0300

    Avances en guia

--- java-itba/guias/introduccion-poo-java/Ej4/Main.java
+++ java-itba/guias/introduccion-poo-java/Ej4/Main.java
@@ -2,7 +2,10 @@ package Ej4;
 
 public class Main {
   public static void main(String[] args) {
-    // Se inicializa una instancia de B -> Se va a usar el method de B
+    // Se inicializa un dato ClassA pero como una instancia de ClassB
+    // Entonces los metodos availables van a ser los de ClassA pero si ClassB
+    // sobreescribio un metodo de ClassA, se va a usar el de ClassB
+    // por lo tanto a.method(3) va a usar el method de B
     ClassA a = new ClassB();
     a.method(3);
 
@@ -18,12 +21,18 @@ public class Main {
     b = new ClassB();
     b.method((Number)3);
 
-    a = new ClassA();
-    b = (ClassB)a;
-    b.method(3);
+    // Esto deberia tirar un error (?
+    // pues a es la clase padre y no puede transformarse en ClassB, ya que es hija
+    // y podria funcionar al revez pero de esta forma no deberia
+    // a = new ClassA();
+    // b = (ClassB)a;
+    // b.method(3);
 
+    // Esto esta bien porque b es de tipo ClassB y puede castearse a una clase
+    // padre
+    // No estoy seguro si a.method va a usar el de A o el de B
     b = new ClassB();
-    a = (ClassA)b;
+    a = (ClassA)b; // Esto hace que se interprete como una instancia de A por lo que si se quiere acceder a method, va a ir al de A
     a.method(3);
   }
 }
---
+++ java-itba/guias/introduccion-poo-java/Ej5/B.java
@@ -0,0 +1,8 @@
+package Ej4;
+
+public class B extends A {
+  @Override
+  public void print(Number num) {
+    System.out.println(MESSAGE.formatted("B","Number",num,num.getClass()));
+  }
+}
