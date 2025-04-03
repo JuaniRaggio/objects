427b01d1062ec865b28667eb7b7024ba06bcca53
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 19:24:14 -0300

    Comparator y comparable - Taller

--- java-itba/talleres-poo/src/main/java/com/talleres/tallerComparable/PhoneNumber.java
+++ java-itba/talleres-poo/src/main/java/com/talleres/tallerComparable/PhoneNumber.java
@@ -4,17 +4,24 @@ public class PhoneNumber implements Comparable<PhoneNumber> {
 
   private int areaCode, prefix, lineNumber;
 
+  public PhoneNumber(int areaCode, int prefix, int lineNumber) {
+    this.areaCode = areaCode;
+    this.prefix = prefix;
+    this.lineNumber = lineNumber;
+  }
+
   public int getAreaCode() { return areaCode; }
   public int getPrefix() { return prefix; }
   public int getLineNumber() { return lineNumber; }
-
+  @Override
+  public String toString() { 
+    return String.format("+%d %d %d", prefix, areaCode, lineNumber);
+  }
   public int compareTo(PhoneNumber that) {
-
     int result = Integer.compare(this.areaCode, that.getAreaCode());
-    if (result == 0) result = Integer.compare(this.prefix, that.getPrefix());
-    if (result == 0) result = Integer.compare(this.lineNumber, that.getLineNumber());
+    if (result == 0) result = Integer.compare(this.prefix, that.prefix);
+    if (result == 0) result = Integer.compare(this.lineNumber, that.lineNumber);
     return result;
-
   }
 
 }
---
+++ java-itba/talleres-poo/src/test/java/com/talleres/tallerComparable/PhoneNumberTest.java
@@ -0,0 +1,23 @@
+package com.talleres.tallerComparable;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.jupiter.api.Test;
+
+public class PhoneNumberTest {
+
+  @Test
+  public void pnTest() {
+    PhoneNumber[] pn = new PhoneNumber[3];
+    pn[0] = new PhoneNumber(11, 549, 10000000);
+    pn[1] = new PhoneNumber(11, 49, 10000000);
+    pn[2] = new PhoneNumber(11, 54, 10000000);
+    Arrays.sort(pn);
+
+    // Mejor forma:
+    Comparator<PhoneNumber> comp = (a, b) -> (b.compareTo(a));
+    Arrays.sort(pn, comp);
+  }
+
+}
