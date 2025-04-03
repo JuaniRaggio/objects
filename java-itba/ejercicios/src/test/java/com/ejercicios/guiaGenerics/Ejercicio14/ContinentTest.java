96d55009d31726c0a8fc7699ac4f388fb6f20ff3
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 15:43:53 -0300

    Ejercicio 14 - Continets

---
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio14/ContinentTest.java
@@ -0,0 +1,13 @@
+package com.ejercicios.guiaGenerics.Ejercicio14;
+
+public class ContinentTest {
+
+   public static void main(String[] args) {
+       System.out.println("Densidades de población:");
+       for(Continent continent : Continent.............) {
+           System.out.println("%s = %.2f".formatted(continent, continent.getPopulationRatio()));
+       }
+       System.out.printf("%.2f", Continent..........("AMERICA").getPopulationRatio());
+   }
+
+}
