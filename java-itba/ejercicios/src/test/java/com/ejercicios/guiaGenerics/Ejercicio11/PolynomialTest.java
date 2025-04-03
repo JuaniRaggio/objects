a7d937f2accda26e1797717274dce5892ebf97f0
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-02 21:45:53 -0300

    Agregamos el try catch enorme para no tener que poner la anotation en el main

--- java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio11/PolynomialTest.java
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio11/PolynomialTest.java
@@ -6,21 +6,20 @@ import org.junit.jupiter.api.Test;
 public class PolynomialTest {
 
   @Test
-  public void polyTest() throws InvalidGradeException, InvalidIndexException {
-    Polynomial fourthGradePol = new Polynomial(4);
-    fourthGradePol.set(2, 3.1); // 3.1 * x^2
-    fourthGradePol.set(3, 2); // 2 * x^3 + 3.1 * x^2
-    assertEquals(fourthGradePol.eval(2), 28.4); // 28.4
-    assertEquals(new Polynomial(3).eval(5), 0); // 0
+  public void polyTest() {
+    // Para evitar tener que poner las anotations hay que hacer un try catch
+    // enorme, ya que si hacemos uno por separado para cada uno, no detecta 
+    // fourthGradePol. Pues puede que la variable no se haya inicializado o
+    // que no este declarada
     try {
+      Polynomial fourthGradePol = new Polynomial(4);
+      fourthGradePol.set(2, 3.1); // 3.1 * x^2
+      fourthGradePol.set(3, 2); // 2 * x^3 + 3.1 * x^2
+      assertEquals(fourthGradePol.eval(2), 28.4); // 28.4
+      assertEquals(new Polynomial(3).eval(5), 0); // 0
       new Polynomial(-4);
-    } catch (InvalidGradeException e) {
-      assertEquals(e.getMessage(), "Grado Invalido"); // 0
-    }
-    try {
       fourthGradePol.set(7, 1.5); // 1.5 * x^7
-    } catch (InvalidIndexException e) {
-      assertEquals("Indice Invalido", e.getMessage());
+    } catch (InvalidIndexException | InvalidGradeException e) {
     }
   }
 
