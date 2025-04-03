6792dd028c48b03866229b58b0490bc931ebc4e5
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 19:44:28 -0300

    Agrego prints para ver si da bien

--- java-itba/talleres-poo/src/test/java/com/talleres/tallerComparable/PhoneNumberTest.java
+++ java-itba/talleres-poo/src/test/java/com/talleres/tallerComparable/PhoneNumberTest.java
@@ -14,10 +14,14 @@ public class PhoneNumberTest {
     pn[1] = new PhoneNumber(11, 49, 10000000);
     pn[2] = new PhoneNumber(11, 54, 10000000);
     Arrays.sort(pn);
+    // for (PhoneNumber p : pn)
+    //   System.out.println(p);
 
     // Mejor forma:
     Comparator<PhoneNumber> comp = (a, b) -> (b.compareTo(a));
     Arrays.sort(pn, comp);
+    // for (PhoneNumber p : pn)
+    //   System.out.println(p);
   }
 
 }
