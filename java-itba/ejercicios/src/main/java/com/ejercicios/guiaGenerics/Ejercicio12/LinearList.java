6caaa0196e8b37e5b3abec8d10653462deffed92
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 14:28:44 -0300

    Dudas Ejercicio 12

---
+++ java-itba/ejercicios/src/main/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearList.java
@@ -0,0 +1,38 @@
+package com.ejercicios.guiaGenerics.Ejercicio12;
+
+// Preguntar porque el ejercicio 12 pide hacer un test antes de hacer la impl
+// de la interfaz
+
+public interface LinearList {
+
+   /**
+    * Agrega un elemento al final de la lista.
+    */
+   void add(Object obj);
+
+   /**
+    * Obtiene el i-ésimo elemento de la lista.
+    */
+   Object get(int i);
+
+   /**
+    * Modifica el i-ésimo elemento de la lista colocando un nuevo valor.
+    */
+   void set(int i, Object obj);
+
+   /**
+    * Elimina el i-ésimo elemento de la lista.
+    */
+   void remove(int i);
+
+   /**
+    * Busca el índice de la primer ocurrencia de un objeto en la lista.
+    */
+   int indexOf(Object obj);
+
+   /**
+    * Retorna el tamaño de la lista.
+    */
+   int size();
+
+}
---
+++ java-itba/ejercicios/src/main/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListImplementation.java
@@ -0,0 +1,13 @@
+// package com.ejercicios.guiaGenerics.Ejercicio12;
+//
+// public class LinearListImplementation implements LinearList {
+//
+//   private Object[] linearL;
+//
+//   public LinearListImplementation() {}
+//
+//    void add(Object obj) {
+//
+//   }
+//
+// }
