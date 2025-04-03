f1015bcbe3af6d0a7cbf76bbfebdc983c4f25111
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-02 21:58:11 -0300

    Reorganizando directorios
    Reorganizacion de directorios para tener los nuevos talleres en un nuevo
    proyecto de maven con JUnit

---
+++ java-itba/talleres-poo/src/main/java/com/talleres/App.java
@@ -0,0 +1,13 @@
+package com.talleres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
