361238803a82b7a9ae2ddb6ff23830c30812ca4d
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 14:29:01 -0300

    Dudas ejercicio 12

---
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListTest.java
@@ -0,0 +1,17 @@
+// package com.ejercicios.guiaGenerics.Ejercicio12;
+//
+// import org.junit.jupiter.api.Test;
+//
+// public class LinearListTest {
+//
+//   @Test
+//   public void linearList() {
+//     LinearList oList = new LinearList() {
+//     };
+//
+//     add(oList);
+//
+//   }
+//
+// }
+
