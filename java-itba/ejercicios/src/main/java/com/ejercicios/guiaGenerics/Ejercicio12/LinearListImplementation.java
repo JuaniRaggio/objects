64794cfbeeb14f847873f47f6ca1750c6f525ad0
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-03 22:13:46 -0300

    LinearList - Ejercicio 12

--- java-itba/ejercicios/src/main/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListImplementation.java
+++ java-itba/ejercicios/src/main/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListImplementation.java
@@ -1,13 +1,66 @@
-// package com.ejercicios.guiaGenerics.Ejercicio12;
-//
-// public class LinearListImplementation implements LinearList {
-//
-//   private Object[] linearL;
-//
-//   public LinearListImplementation() {}
-//
-//    void add(Object obj) {
-//
-//   }
-//
-// }
+package com.ejercicios.guiaGenerics.Ejercicio12;
+
+import java.util.Arrays;
+
+public class LinearListImplementation implements LinearList {
+
+  private Object[] linearL;
+
+  private int elementsInList, size;
+
+  private final int stepSize = 10;
+
+  public LinearListImplementation(int initialSize) {
+    size = initialSize;
+    linearL = new Object[size];
+    elementsInList = 0;
+  }
+
+  public void add(Object obj) {
+    if (elementsInList == size) {
+      linearL = Arrays.copyOf(linearL, size + stepSize);
+    }
+    linearL[elementsInList++] = obj;
+  }
+
+  public Object get(int i) {
+    try {
+      return linearL[i];
+    } catch (IndexOutOfBoundsException e) {
+      System.err.println(e.getMessage());
+      return null;
+    }
+  }
+
+  public void set(int i, Object obj) {
+    try {
+      linearL[i] = obj;
+      if (obj == null) elementsInList--;
+    } catch (IndexOutOfBoundsException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public void remove(int i) {
+    try {
+      if (linearL[i] != null) {
+        linearL[i] = null;
+        elementsInList--;
+      }
+    } catch (IndexOutOfBoundsException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  public int indexOf(Object obj) {
+    for (int i = 0; i < size; ++i) {
+      if (linearL[i] == obj) return i;
+    }
+    return -1;
+  }
+
+  public int size() {
+    return elementsInList;
+  }
+
+}
--- java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListTest.java
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio12/LinearListTest.java
@@ -1,17 +1,60 @@
-// package com.ejercicios.guiaGenerics.Ejercicio12;
-//
-// import org.junit.jupiter.api.Test;
-//
-// public class LinearListTest {
-//
-//   @Test
-//   public void linearList() {
-//     LinearList oList = new LinearList() {
-//     };
-//
-//     add(oList);
-//
-//   }
-//
-// }
+package com.ejercicios.guiaGenerics.Ejercicio12;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LinearListTest {
+
+  @Test
+  public void linearList() {
+    LinearList list = new LinearListImplementation(5);
+    Object o1 = new Object();
+    Object o2 = new Object();
+    Object o3 = new Object();
+    Object o4 = new Object();
+    Object o5 = new Object();
+    list.add(o1);
+    assertEquals(list.size(), 1);
+    assertEquals(list.get(0), o1);
+    assertEquals(list.indexOf(o1), 0);
+    assertEquals(list.get(1), null);
+    list.set(0, o2);
+    assertEquals(list.get(0), o2);
+    assertEquals(list.indexOf(o2), 0);
+    list.set(0, null);
+    assertEquals(list.size(), 0);
+    list.remove(2);
+    assertEquals(list.size(), 0);
+    list.add(o2);
+    list.add(o4);
+    list.add(o3);
+    list.add(o1);
+    assertEquals(list.get(0), o2);
+    assertEquals(list.indexOf(o2), 0);
+    assertEquals(list.get(1), o4);
+    assertEquals(list.indexOf(o4), 1);
+    assertEquals(list.get(2), o3);
+    assertEquals(list.indexOf(o3), 2);
+    assertEquals(list.get(3), o1);
+    assertEquals(list.indexOf(o1), 3);
+    assertEquals(list.get(4), null);
+    assertEquals(list.size(), 4);
+    list.add(o5);
+    assertEquals(list.size(), 5);
+    assertEquals(list.get(4), o5);
+    assertEquals(list.indexOf(o5), 4);
+    assertEquals(list.get(5), null);
+    assertEquals(list.get(500), null);
+    list.remove(5);
+    assertEquals(list.size(), 5);
+    list.remove(4);
+    assertEquals(list.size(), 4);
+    list.remove(4);
+    assertEquals(list.size(), 4);
+    list.remove(3);
+    assertEquals(list.size(), 3);
+  }
+
+}
 
