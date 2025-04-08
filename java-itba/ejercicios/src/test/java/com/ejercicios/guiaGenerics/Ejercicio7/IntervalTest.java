4c7f482638a67923524dd6785ce777d2130df4bd
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-08 00:50:00 -0300

    IntervalTest

---
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaGenerics/Ejercicio7/IntervalTest.java
@@ -0,0 +1,24 @@
+// package com.ejercicios.guiaGenerics.Ejercicio7;
+//
+// import org.junit.jupiter.api.Test;
+//
+// public class IntervalTest {
+//
+//   @Test
+//   public void main() {
+//     Interval interval = new Interval(-1, 1, 0.5);
+//     System.out.println(interval);
+//     System.out.println(interval.size());
+//     System.out.println(interval.at(3));
+//     System.out.println(interval.indexOf(0.5));
+//     System.out.println(interval.includes(0.5));
+//     System.out.println(interval.equals(new Interval(-1, 1, 0.5)));
+//     System.out.println(interval.hashCode() == new Interval(-1, 1, 0.5).hashCode());
+//     // System.out.println(interval.count(new IntervalCondition() {
+//       public boolean satisfies(double value) {
+//         return value > 0;
+//       }
+//     }));
+//   }
+//
+// }
