a67b7ee9c1ce5520a583040f52f90afccb0ef432
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-11 01:06:14 -0300

    PairTest - Falta hacerlo pero hay un par de cosas que no las entendi, se sigue despues

---
+++ java-itba/ejercicios/src/test/java/com/ejercicios/guiaIterables/Ejercicio1/PairTest.java
@@ -0,0 +1,21 @@
+package com.ejercicios.guiaIterables.Ejercicio1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PairTest {
+
+  @Test
+  public void pairTest() {
+    Pair<String, Double> stringDoublePair = new Pair<>("hola", 0.1);
+    assertEquals("{hola, 0.1}", stringDoublePair);
+    Pair<Integer, Integer> integerIntegerPair = new Pair<>(1, 2);
+    assertEquals("{1, 2}", integerIntegerPair);
+    assertTrue(stringDoublePair.equals(new Pair<>("hola", 0.1)));
+    Pair otherPair = new Pair(1f, "mundo");
+    System.out.println(otherPair);
+  }
+  
+}
