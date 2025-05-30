4885e7d184d5fb5195ea252c0d835dae181a469d
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-05-30 18:19:56 -0300

    Expressions hecho con exito

--- ruby-itba/tp10_solucion/expressions.rb
+++ ruby-itba/tp10_solucion/expressions.rb
@@ -1,23 +1,75 @@
 #!/usr/bin/env ruby
-
 module Expression
- def evaluate
-   raise 'Not implemented'
- end
+  def evaluate
+    raise 'Not implemented'
+  end
+
+  def not
+    Not.new(self)
+  end
+
+  def or(other_exp)
+    Or.new(self, other_exp)
+  end
+
+  def and(other_exp)
+    And.new(self, other_exp)
+  end
 end
 
 class SimpleExpression
- include Expression
+  include Expression
 
- attr_writer :value
+  attr_writer :value
 
- def initialize(value)
-   @value = value
- end
+  def initialize(value)
+    @value = value
+  end
 
- def evaluate
-   @value
- end
+  def evaluate
+    @value
+  end
+end
+
+class BinaryOperation
+  include Expression
+  def initialize(exp1, exp2)
+    @left_exp = exp1
+    @right_exp = exp2
+  end
+
+  protected
+
+  attr_reader :left_exp, :right_exp
+end
+
+class And < BinaryOperation
+  def evaluate
+    @left_exp.evaluate && @right_exp.evaluate
+  end
+end
+
+class Or < BinaryOperation
+  def evaluate
+    @left_exp.evaluate || @right_exp.evaluate
+  end
+end
+
+class UnaryOperation
+  include Expression
+  def initialize(exp)
+    @expression = exp
+  end
+
+  protected
+
+  attr_reader :expression
+end
+
+class Not < UnaryOperation
+  def evaluate
+    !@expression.evaluate
+  end
 end
 
 # Test
@@ -34,5 +86,5 @@ exp1.value = false
 p exp3.evaluate # true
 p exp4.evaluate # false
 p exp5.evaluate # false
-exp2.value = true
+p exp2.value = true
 p exp5.evaluate # true
